bd59421a78cdf9ddda26ed18c27abfd7174c9b9e
viocha <viocha@qq.com>
2024-07-05 21:35:38 +0800

    2024/7/5 21:35:37

--- build/create-index.js
+++ build/create-index.js
@@ -2,8 +2,9 @@ import fs from 'fs';
 import path from 'path';
 const __dirname = import.meta.dirname;
 
-const idxPath = path.resolve(__dirname, '../index.html');
-const htmlPath = path.resolve(__dirname, '../html');
+const idxPath = path.join(__dirname, '../index.html');
+const htmlPath = path.join(__dirname, '../html');
+const rootPath = path.join(__dirname, '../');
 
 let html = '';
 scanHtmlFiles(htmlPath);
@@ -14,12 +15,12 @@ function scanHtmlFiles(dir){
 		const fullA = path.join(dir, a);
 		const fullB = path.join(dir, b);
 		return fs.statSync(fullA).isDirectory()-fs.statSync(fullB).isDirectory();
-		
 	}).forEach(name=>{
 		const fullName = path.join(dir, name);
+		const pathFromRoot=path.relative(rootPath, fullName)
 		if (fs.statSync(fullName).isFile() && fullName.endsWith('.html')){
 			const indent = fullName.match(/\\/g)?.length || 0;
-			html += `<div style="padding-left:${indent}em;"><a href="${fullName}">${fullName}</a></div>`;
+			html += `<div style="padding-left:${indent}em;"><a href="${pathFromRoot}">${pathFromRoot}</a></div>`;
 		} else if (fs.statSync(fullName).isDirectory()){
 			scanHtmlFiles(fullName);
 		}
